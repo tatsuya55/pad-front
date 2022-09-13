21d0fb737d8c976dd787c540435ca012106b1e2f
小先生 <1761970318@qq.com>
2022-09-13 09:24:59 +0800

    修改页面

--- src/main/java/com/pad/controller/CompanyDetailController.java
+++ src/main/java/com/pad/controller/CompanyDetailController.java
@@ -1,6 +1,10 @@
 package com.pad.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @author F4
  * @since 2022-09-12
  */
-@RestController
+@Api(tags = "详情页")
+@Controller
 @RequestMapping("/company-detail")
 public class CompanyDetailController {
+
+    @ApiOperation("详细信息")
+    @GetMapping("/details")
+    public String toSignIn(){
+        return "news-details";
+    }
 
 }
 
