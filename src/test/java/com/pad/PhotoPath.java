4dd49810dab8134b6a03476c163741a112886a8d
小先生 <1761970318@qq.com>
2022-09-16 19:14:09 +0800

    上传材料

--- src/test/java/com/pad/PhotoPath.java
+++ src/test/java/com/pad/PhotoPath.java
@@ -3,7 +3,7 @@ package com.pad;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.ClassUtils;
 
-public class qqq {
+public class PhotoPath {
     @Test
     public void test(){
         String path = ClassUtils.getDefaultClassLoader().getResource("").getPath();
