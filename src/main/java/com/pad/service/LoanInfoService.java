4ea76f273abe9f5521ff25fea87f4bfb3d2c9fe4
Tatsuya <2506682863@qq.com>
2022-09-27 14:54:49 +0800

    提交贷款申请步骤条

--- src/main/java/com/pad/service/LoanInfoService.java
+++ src/main/java/com/pad/service/LoanInfoService.java
@@ -16,4 +16,6 @@ import java.util.List;
 public interface LoanInfoService extends IService<LoanInfo> {
     /*根据企业编号 查询贷款信息*/
     List<LoanInfo> findBy(String cNo);
+    /*判断贷款申请是否审核*/
+    Integer selectStatus(String cNo);
 }
