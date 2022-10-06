5201d56d32770c8159f94898c54c17e720c359d7
小先生 <1761970318@qq.com>
2022-10-06 18:09:20 +0800

    沙箱支付

--- src/main/java/com/pad/controller/AliPayController.java
+++ src/main/java/com/pad/controller/AliPayController.java
@@ -6,12 +6,9 @@ import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
 import com.alipay.api.internal.util.AlipaySignature;
 import com.alipay.api.request.AlipayTradePagePayRequest;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
 import com.pad.alipay.AliPay;
 import com.pad.alipay.AliPayConfig;
-import com.pad.entity.Periodization;
-import com.pad.mapper.PeriodizationMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
+
 import java.util.HashMap;
 import java.util.Map;
 
+// xjlugv6874@sandbox.com
+// 9428521.24 - 30 = 9428491.24 + 30 = 9428521.24
 @RestController
 @RequestMapping("/alipay")
 public class AliPayController {
+
     private static final String GATEWAY_URL = "https://openapi.alipaydev.com/gateway.do";
     private static final String FORMAT = "JSON";
     private static final String CHARSET = "UTF-8";
@@ -36,9 +36,6 @@ public class AliPayController {
     @Resource
     private AliPayConfig aliPayConfig;
 
-    @Autowired
-    private PeriodizationMapper periodizationMapper;
-    
 
     @GetMapping("/pay") // &subject=xxx&traceNo=xxx&totalAmount=xxx
     public void pay(AliPay aliPay, HttpServletResponse httpResponse) throws Exception {
@@ -49,6 +46,7 @@ public class AliPayController {
         // 2. 创建 Request并设置Request参数
         AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();  // 发送请求的 Request类
         request.setNotifyUrl(aliPayConfig.getNotifyUrl());
+        request.setReturnUrl(aliPayConfig.getReturnUrl());
         JSONObject bizContent = new JSONObject();
         bizContent.set("out_trade_no", aliPay.getTraceNo());  // 我们自己生成的订单编号
         bizContent.set("total_amount", aliPay.getTotalAmount()); // 订单的总金额
@@ -100,18 +98,11 @@ public class AliPayController {
                 System.out.println("买家付款时间: " + params.get("gmt_payment"));
                 System.out.println("买家付款金额: " + params.get("buyer_pay_amount"));
 
-                // 查询订单
-                QueryWrapper<Periodization> queryWrapper = new QueryWrapper<>();
-                queryWrapper.eq("id", outTradeNo);
-                Periodization periodization = periodizationMapper.selectOne(queryWrapper);
-
-                if (periodization != null) {
-                    periodization.setRepaymentTime(new Date());
-                    periodization.setStatus(1);
-                    periodizationMapper.updateById(periodization);
-                }
             }
         }
         return "success";
     }
+
 }
+
+
